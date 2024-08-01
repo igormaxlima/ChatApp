d65d6d214da989f09d38cd8480f359a6a55201ee
igormaxlima <igormaxnunes@gmail.com>
2024-08-01 17:37:23 -0300

    fix: adding inbox view empty state

--- ChatApp/Core/Inbox/View/InboxView.swift
+++ ChatApp/Core/Inbox/View/InboxView.swift
@@ -19,73 +19,84 @@ struct InboxView: View {
     
     var body: some View {
         NavigationStack {
-            List {
-                ActiveNowView()
-                    .listRowSeparator(.hidden)
-                    .listRowInsets(EdgeInsets())
-                    .padding(.vertical)
-                    .padding(.horizontal, 4)
-                
-                ForEach(inboxViewModel.recentMessages) { message in
-                    ZStack {
-                        NavigationLink(value: message) {
-                            EmptyView()
-                        }.opacity(0.0)
-                        
-                        InboxRowView(message: message)
-                    }
-                }
-            }
-            .navigationTitle("Chats")
-            .navigationBarTitleDisplayMode(.inline)
-            .listStyle(PlainListStyle())
-            .onChange(of: selectedUser, { _, newValue in
-                showChat = newValue != nil
-            })
-            // for: tipo de objeto que sera passado para o destino da navegacao
-            // when you get this piece of data (for), do this (destination)
-            .navigationDestination(for: Route.self, destination: { route in
-                switch route {
-                case .profile(let user):
-                    ProfileView(user: user)
-                case .chatView(let user):
-                    ChatView(user: user)
-                }
-            })
-            .navigationDestination(for: Message.self, destination: { message in
-                if let user = message.user {
-                    ChatView(user: user)
-                }
-            })
-            .navigationDestination(isPresented: $showChat, destination: {
-                if let user = selectedUser {
-                    ChatView(user: user)
-                }
-            })
-            .fullScreenCover(isPresented: $isShowingNewMessageView, content: {
-                NewMessageView(selectedUser: $selectedUser)
-            })
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    // value: parsing data through the navigation
-                    if let user {
-                        NavigationLink(value: Route.profile(user)) {
-                            CircularProfileImageView(user: user, size: .xSmall)
+            if ((inboxViewModel.currentUser == nil) && inboxViewModel.recentMessages.isEmpty) {
+                ProgressView("Loading...")
+                    .progressViewStyle(CircularProgressViewStyle())
+            } else {
+                List {
+                    ActiveNowView()
+                        .listRowSeparator(.hidden)
+                        .listRowInsets(EdgeInsets())
+                        .padding(.vertical)
+                        .padding(.horizontal, 4)
+                    
+                    ForEach(inboxViewModel.recentMessages) { message in
+                        ZStack {
+                            NavigationLink(value: message) {
+                                EmptyView()
+                            }.opacity(0.0)
+                            
+                            InboxRowView(message: message)
                         }
                     }
                 }
-                
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        isShowingNewMessageView.toggle()
-                        selectedUser = nil
-                    } label: {
-                        Image(systemName: "square.and.pencil.circle.fill")
-                            .resizable()
-                            .frame(width: 32, height: 32)
-                            .foregroundStyle(.black, Color(.systemGray5))
+                .navigationTitle("Chats")
+                .navigationBarTitleDisplayMode(.inline)
+                .listStyle(PlainListStyle())
+                .onChange(of: selectedUser, { _, newValue in
+                    showChat = newValue != nil
+                })
+                // for: tipo de objeto que sera passado para o destino da navegacao
+                // when you get this piece of data (for), do this (destination)
+                .navigationDestination(for: Route.self, destination: { route in
+                    switch route {
+                    case .profile(let user):
+                        ProfileView(user: user)
+                    case .chatView(let user):
+                        ChatView(user: user)
+                    }
+                })
+                .navigationDestination(for: Message.self, destination: { message in
+                    if let user = message.user {
+                        ChatView(user: user)
+                    }
+                })
+                .navigationDestination(isPresented: $showChat, destination: {
+                    if let user = selectedUser {
+                        ChatView(user: user)
+                    }
+                })
+                .fullScreenCover(isPresented: $isShowingNewMessageView, content: {
+                    NewMessageView(selectedUser: $selectedUser)
+                })
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        // value: parsing data through the navigation
+                        if let user {
+                            NavigationLink(value: Route.profile(user)) {
+                                CircularProfileImageView(user: user, size: .xSmall)
+                            }
+                        }
+                    }
+                    
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            isShowingNewMessageView.toggle()
+                            selectedUser = nil
+                        } label: {
+                            Image(systemName: "square.and.pencil.circle.fill")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                                .foregroundStyle(.black, Color(.systemGray5))
+                        }
                     }
                 }
+                .overlay {
+                    if (inboxViewModel.recentMessages.isEmpty) {
+                        ContentUnavailableView("No Chats", systemImage: "bubble.left.and.exclamationmark.bubble.right", description: Text("Tap on a user to start a chat."))
+                    }
+                }
+                
             }
         }
     }
